5078b05e8b3c62002ba014889e785480c570ab84
elmachrafimouad <58666531+elmachrafimouad@users.noreply.github.com>
2022-09-11 16:18:05 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -820,53 +820,11 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-//               <div hidden="" id="faq" className="faq">
-//                 <div className="head-line">
-//                   <h1>FAQ</h1>
-//                 </div>
-//                 <p className="first-p">
-//                   What is ShibaPlayer
-//                   <svg
-//                     stroke="currentColor"
-//                     fill="currentColor"
-//                     strokeWidth={0}
-//                     viewBox="0 0 512 512"
-//                     height="1em"
-//                     width="1em"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z" />
-//                   </svg>
-//                 </p>
-//                 <p className="first-p">
-//                   Where can I purchase a ShibaPlayer NFT?
-//                   <svg
-//                     stroke="currentColor"
-//                     fill="currentColor"
-//                     strokeWidth={0}
-//                     viewBox="0 0 512 512"
-//                     height="1em"
-//                     width="1em"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z" />
-//                   </svg>
-//                 </p>
-//                 <p className="first-p">
-//                   What's the ShibaPlayer future?
-//                   <svg
-//                     stroke="currentColor"
-//                     fill="currentColor"
-//                     strokeWidth={0}
-//                     viewBox="0 0 512 512"
-//                     height="1em"
-//                     width="1em"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z" />
-//                   </svg>
-//                 </p>
-//               </div>
+             <div hidden="" id="faq" className="faq">
+            
+            
+           
+              </div>
             </div>
             <div className="footer">
               <h1>ShibaPlayer community</h1>
